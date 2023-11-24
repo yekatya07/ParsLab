d2487ee617a9c52d8d1822fc17a15714d3d03a24
Ekaterina <katerin22502@gmail.ru>
2023-11-24 17:15:52 +0700

    Добавьте файлы проекта.

---
+++ DB/DataBase.cs
@@ -0,0 +1,63 @@
+﻿using DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class DataBase
+    {
+        testingDBEntities db = new testingDBEntities();
+
+        public info GetByID(int id)
+        {
+            return db.info.FirstOrDefault(x => x.id == id);
+        }
+
+        public info GetByName(string name)
+        {
+            return db.info.FirstOrDefault(x => x.name == name);
+        }
+
+        public void Add(int id, string message, string name)
+        {
+            if (!CheckID(id)) return;
+
+            info inf = new info();
+            inf.id = id;
+            inf.name = name;
+            inf.message = message;
+
+            db.info.Add(inf);
+            db.SaveChanges();
+        }
+
+        public void Update(int id, string newMessage)
+        {
+            info inf = GetByID(id);
+            if (inf != null)
+            {
+                inf.message = newMessage;
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            info inf = GetByID(id);
+            if (inf != null)
+            {
+                db.info.Remove(inf);
+                db.SaveChanges();
+            }
+        }
+
+        public bool CheckID(int id)
+        {
+            if (db.info.FirstOrDefault(x => x.id == id) != null) return true;
+            return false;
+        }
+    }
+}
---
+++ ParsLab/Program.cs
@@ -0,0 +1,30 @@
+﻿using DB;
+
+namespace ParsLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase db = new DataBase();
+            db.Add(1, "", "");
+            //IWebDriver driverr = new ChromeDriver(); // Создание объекта-браузера
+            //driverr.Navigate().GoToUrl("https://pikabu.ru/best");
+            //var articles = driverr.FindElements(By.ClassName("story"));
+
+            //Console.WriteLine(articles.Count);
+
+            //foreach (var article in articles)
+            //{
+            //    var message = article.FindElement(By.ClassName("story__title")).Text;
+            //    var name = article.GetAttribute("data-author-name");
+            //    var id = article.GetAttribute("data-story-id");
+
+            //    Console.WriteLine($"{id} - {name} - {message}");
+
+            //    //DB bd = new DB();
+            //    //bd.Add(int.Parse(id), message, name);
+            //}
+        }
+    }
+}
